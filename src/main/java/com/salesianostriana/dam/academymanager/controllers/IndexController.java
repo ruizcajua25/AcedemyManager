0b95aceaa8d068185518bda1bda10905c711eb3f
juanrur <juanruizcampa@gmail.com>
2026-05-13 12:05:01 +0200

    add: index y index controller

--- src/main/java/com/salesianostriana/dam/academymanager/controllers/IndexController.java
+++ src/main/java/com/salesianostriana/dam/academymanager/controllers/IndexController.java
@@ -1,7 +1,11 @@
 package com.salesianostriana.dam.academymanager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.salesianostriana.dam.academymanager.modules.Profesor;
+
 
 @Controller
 public class IndexController {
@@ -9,5 +13,11 @@ public class IndexController {
   @GetMapping("/")
   public String index () {
     return "index.html";
+  }
+
+  @GetMapping("/formulario")
+  public String formulario(Model model) {
+    model.addAttribute("profesor", new Profesor());
+    return "formulario";
   }
 }
