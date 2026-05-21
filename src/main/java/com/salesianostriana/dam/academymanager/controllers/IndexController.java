b5e6d1759361fc6695558b29f51edabc6174de4c
juanrur <juanruizcampa@gmail.com>
2026-05-21 09:35:15 +0200

    fix: usuario repository id mal puesto

--- src/main/java/com/salesianostriana/dam/academymanager/controllers/IndexController.java
+++ src/main/java/com/salesianostriana/dam/academymanager/controllers/IndexController.java
@@ -6,18 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.salesianostriana.dam.academymanager.modules.Profesor;
 
-
 @Controller
 public class IndexController {
-  
   @GetMapping("/")
   public String index () {
     return "index.html";
   }
 
-  @GetMapping("/formulario")
-  public String formulario(Model model) {
+
+  @GetMapping("/login")
+  public String login(Model model) {
     model.addAttribute("profesor", new Profesor());
-    return "formulario";
+    return "login";
   }
 }
