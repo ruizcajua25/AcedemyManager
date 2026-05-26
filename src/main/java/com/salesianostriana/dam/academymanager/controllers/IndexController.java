1ca093e0ee616ce91714b9a037bf1fd552e34b91
juanrur <juanruizcampa@gmail.com>
2026-05-26 09:53:09 +0200

    add: registro y base service a use service

--- src/main/java/com/salesianostriana/dam/academymanager/controllers/IndexController.java
+++ src/main/java/com/salesianostriana/dam/academymanager/controllers/IndexController.java
@@ -1,14 +1,25 @@
 package com.salesianostriana.dam.academymanager.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.salesianostriana.dam.academymanager.modules.Profesor;
+import com.salesianostriana.dam.academymanager.modules.Usuario;
+import com.salesianostriana.dam.academymanager.services.UsuarioService;
 
 
 @Controller
 public class IndexController {
+  @Autowired
+  private UsuarioService userService;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   @GetMapping("/")
   public String index () {
     return "index.html";
@@ -17,7 +28,20 @@ public class IndexController {
 
   @GetMapping("/login")
   public String login(Model model) {
-    model.addAttribute("profesor", new Profesor());
+    model.addAttribute("usuario", new Usuario());
     return "login";
+  }
+
+  @GetMapping("/registro")
+  public String registro(Model model) {
+    model.addAttribute("usuario", new Usuario());
+    return "registro";
+  }
+
+  @PostMapping("/registro")
+  public String procesarRegistro(@ModelAttribute("usuario") Usuario usuario) {
+    usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+    userService.save(usuario);
+    return "redirect:/login";
   }
 }
--- src/main/java/com/salesianostriana/dam/academymanager/services/UsuarioService.java
+++ src/main/java/com/salesianostriana/dam/academymanager/services/UsuarioService.java
@@ -6,16 +6,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.salesianostriana.dam.academymanager.modules.Usuario;
 import com.salesianostriana.dam.academymanager.repositories.UsuarioRepository;
 
 @Service
-public class UsuarioService implements UserDetailsService {
+public class UsuarioService extends BaseService<Usuario, Long, UsuarioRepository> implements UserDetailsService {
   @Autowired
   private UsuarioRepository usuarioRepository;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return (UserDetails) usuarioRepository.findByNombre(username)
+    return (UserDetails) usuarioRepository.findByUsername(username)
       .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
   }
 }
