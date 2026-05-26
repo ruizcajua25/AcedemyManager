dacd6ed630ac8169c9b16e7d104ee8cfe2c29d8b
juanrur <juanruizcampa@gmail.com>
2026-05-26 09:21:43 +0200

    fix: después del inicio de session redirige a / en vez de a /home

--- src/main/java/com/salesianostriana/dam/academymanager/security/SecurityConfig.java
+++ src/main/java/com/salesianostriana/dam/academymanager/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
       )
       .formLogin(form -> form
         .loginPage("/login")
-        .defaultSuccessUrl("/home", true)
+        .defaultSuccessUrl("/", true)
         .permitAll()
       )
       .logout(logout -> logout
