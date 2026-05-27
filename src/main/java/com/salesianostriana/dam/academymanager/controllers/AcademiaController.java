ff13747d16f9070bb4db78f420a38682f76fa3f9
juanrur <juanruizcampa@gmail.com>
2026-05-27 09:03:13 +0200

    feature: hacer que se puedan crear academias y que el boton de perfil mande a la pagina de crear

--- src/main/java/com/salesianostriana/dam/academymanager/controllers/AcademiaController.java
+++ src/main/java/com/salesianostriana/dam/academymanager/controllers/AcademiaController.java
@@ -1,15 +1,51 @@
 package com.salesianostriana.dam.academymanager.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.salesianostriana.dam.academymanager.modules.Academia;
+import com.salesianostriana.dam.academymanager.modules.Director;
+import com.salesianostriana.dam.academymanager.modules.TipoUsuarioId;
+import com.salesianostriana.dam.academymanager.modules.Usuario;
+import com.salesianostriana.dam.academymanager.services.AcademiaService;
+import com.salesianostriana.dam.academymanager.services.DirectorService;
 
 
 @Controller
 public class AcademiaController {
-  @GetMapping("/academia/add")
-  public String index () {
+  @Autowired
+  private AcademiaService academiaService;
+  @Autowired
+  private DirectorService directorService;
+
+  @GetMapping("/academia/create")
+  public String index (Model model) { 
+    model.addAttribute("academia", new Academia());
     return "academia/create";
+  }
+
+  @PostMapping("/academia/create")
+  public String create (@AuthenticationPrincipal Usuario usuario ,@ModelAttribute Academia academia) {
+    academiaService.save(academia);
+
+    TipoUsuarioId id = TipoUsuarioId.builder()
+      .academiaId(academia.getId())
+      .usuarioId(usuario.getId())
+      .build();
+
+    Director director = Director.builder()
+      .id(id)
+      .academia(academia)
+      .usuario(usuario)
+      .build();
+
+    directorService.save(director);
+    return "redirect:/academias";
   }
 
   @GetMapping("/academias")
