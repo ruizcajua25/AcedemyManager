e73df4c149782df5aa35455900a65fcf544971ec
juanrur <juanruizcampa@gmail.com>
2026-05-27 10:33:23 +0200

    add: alumno service

---
+++ src/main/java/com/salesianostriana/dam/academymanager/services/AlumnoService.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.dam.academymanager.services;
+
+import org.springframework.stereotype.Service;
+
+import com.salesianostriana.dam.academymanager.modules.Alumno;
+import com.salesianostriana.dam.academymanager.modules.TipoUsuarioId;
+import com.salesianostriana.dam.academymanager.repositories.AlumnoRepository;
+
+@Service
+public class AlumnoService extends BaseService<Alumno, TipoUsuarioId, AlumnoRepository> {
+    
+}
