a9751abab92ef6f98ed1ffb5ef1d155d0b4dc510
Juan Ruiz <juanruizcampa@gmail.com>
2026-06-01 02:51:09 +0200

    add: @Repository y @Service a los bean de curso

--- src/main/java/com/salesianostriana/dam/academymanager/repositories/CursoRepository.java
+++ src/main/java/com/salesianostriana/dam/academymanager/repositories/CursoRepository.java
@@ -1,9 +1,11 @@
 package com.salesianostriana.dam.academymanager.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.salesianostriana.dam.academymanager.modules.Curso;
 
+@Repository
 public interface CursoRepository extends JpaRepository<Curso, String> {
   
 }
--- src/main/java/com/salesianostriana/dam/academymanager/services/CursoService.java
+++ src/main/java/com/salesianostriana/dam/academymanager/services/CursoService.java
@@ -1,8 +1,11 @@
 package com.salesianostriana.dam.academymanager.services;
 
+import org.springframework.stereotype.Service;
+
 import com.salesianostriana.dam.academymanager.modules.Curso;
 import com.salesianostriana.dam.academymanager.repositories.CursoRepository;
 
+@Service
 public class CursoService extends BaseService<Curso, String, CursoRepository> {
   
 }
