992b258c3bd7c6c00f8ebcc65d20411798cefeec
Juanr <juanruizcampa@gmail.com>
2026-06-01 20:36:23 +0200

    add: excepciones

---
+++ src/main/java/com/salesianostriana/dam/academymanager/exceptions/AccionNoPermitidaException.java
@@ -0,0 +1,8 @@
+package com.salesianostriana.dam.academymanager.exceptions;
+
+public class AccionNoPermitidaException extends RuntimeException {
+
+  public AccionNoPermitidaException(String mensaje) {
+    super(mensaje);
+  }
+}
---
+++ src/main/java/com/salesianostriana/dam/academymanager/exceptions/ObjetoNoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.salesianostriana.dam.academymanager.exceptions;
+
+public class ObjetoNoEncontradoException extends RuntimeException {
+
+  public ObjetoNoEncontradoException(String mensaje) {
+    super(mensaje);
+  }
+}
