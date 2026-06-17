0d6a093e9196adde2a47e027f5550212615b9676
ruizcajua25 <ruiz.cajua25@triana.salesianos.edu>
2026-06-17 11:44:47 +0200

    add: queries a AcademiaRepository y CursoRepository

--- src/main/java/com/salesianostriana/dam/academymanager/repositories/CursoRepository.java
+++ src/main/java/com/salesianostriana/dam/academymanager/repositories/CursoRepository.java
@@ -1,5 +1,8 @@
 package com.salesianostriana.dam.academymanager.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import com.salesianostriana.dam.academymanager.modules.Curso;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, String> {
-  
+  List<Curso> findByAcademiaId(String academiaId);
+  List<Curso> findByAcademiaIdAndFechaInicioBeforeAndFechaFinAfter(String academiaId, LocalDate today1, LocalDate today2);
+  List<Curso> findByAcademiaIdOrderByNombre(String academiaId);
+  List<Curso> findByAcademiaIdAndProfesoresIsEmpty(String academiaId);
+  List<Curso> findByProfesoresIsEmpty();
 }
