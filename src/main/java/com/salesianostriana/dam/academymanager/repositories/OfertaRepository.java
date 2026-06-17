d0035b7addfca1517a0ae846c7ed48072899fcc3
ruizcajua25 <ruiz.cajua25@triana.salesianos.edu>
2026-06-17 11:45:20 +0200

    add: queries a OfertaRepository para filtrar por academia y candidatos

--- src/main/java/com/salesianostriana/dam/academymanager/repositories/OfertaRepository.java
+++ src/main/java/com/salesianostriana/dam/academymanager/repositories/OfertaRepository.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.dam.academymanager.repositories;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +11,13 @@ import com.salesianostriana.dam.academymanager.modules.Oferta;
 
 @Repository
 public interface OfertaRepository extends JpaRepository<Oferta, String> {
-  public List<Oferta> findByAcademia(Academia academia);
-  public List<Oferta> findByCandidatosId(String id);
-  public boolean existsByIdAndAcademiaDirectoresUsuarioId(String id, String usuarioId);
+  List<Oferta> findByAcademia(Academia academia);
+  List<Oferta> findByAcademiaId(String academiaId);
+  List<Oferta> findByAcademiaIdAndActivaTrue(String academiaId);
+  List<Oferta> findByAcademiaIdAndActivaTrueAndCurso_FechaInicioAfter(String academiaId, LocalDate today);
+  List<Oferta> findByAcademiaIdAndActivaTrueAndCursoIsNull(String academiaId);
+  List<Oferta> findByCandidatosId(String id);
+  List<Oferta> findByCandidatosIdAndActivaTrue(String usuarioId);
+  boolean existsByIdAndAcademiaDirectoresUsuarioId(String id, String usuarioId);
+  boolean existsByIdAndAcademia_Directores_UsuarioId(String ofertaId, String usuarioId);
 }
